bd409c6c8aa94b8fb2c08c45af8f7a5c952ab0ff
Tyler Brock <tyler.brock@gmail.com>
2020-07-03 12:56:03 -0700

    v0.0.3

--- build/prune.js
+++ build/prune.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 "use strict";
 var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
     if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
